4b184cc85e158592a63fe4eae323e0a990a2d5fe
subhan <subhan.nooriansyah@gmail.com>
2020-01-27 14:13:03 +0700

    fixup bug

---
+++ app/readme.md
@@ -0,0 +1,4 @@
+# Android Connection using Receiver Kotlin Version
+> Developing android broadcast receiver.
+
+This is repository for using receiving broadcast connection android offline or online
--- app/src/main/java/id/shuja/demo/MainActivity.kt
+++ app/src/main/java/id/shuja/demo/MainActivity.kt
@@ -3,58 +3,42 @@ package id.shuja.demo
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
 	private lateinit var receiver: NetworkReceiver
 
+	companion object {
+		private lateinit var tvNoInternet:TextView
+
+		var refreshBrowser = true
+	}
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		tvNoInternet = findViewById(R.id.tv_no_internet)
+
 	}
 
 	override fun onStart() {
 		super.onStart()
+		receiver = NetworkReceiver()
+		registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
 
-		val conn = Connectivity.getConnectivityManager(this)
-		val activateInfo: NetworkInfo = conn.activeNetworkInfo
-
-		if (activateInfo.isConnected && activateInfo != null) {
-			wifiConnected = activateInfo.type == ConnectivityManager.TYPE_WIFI
-			mobileConnected = activateInfo.type == ConnectivityManager.TYPE_MOBILE
-		} else {
-			wifiConnected = false
-			mobileConnected = false
-		}
-
-
-		if (refreshBrowser) {
-			if (wifiConnected || mobileConnected) {
-				Toast.makeText(this, "Refresh Internet", Toast.LENGTH_LONG).show()
-			} else {
-				Toast.makeText(this, "Error Page", Toast.LENGTH_LONG).show()
-			}
-		}
 	}
 
 	override fun onDestroy() {
 		super.onDestroy()
 		this.unregisterReceiver(receiver)
-	}
-
-	companion object {
-		const val WIFI ="Wi-Fi"
-		const val MOBILE ="Mobile"
-
-		private var wifiConnected = false
-		private var mobileConnected = false
-
-		var refreshBrowser = true
 	}
 
 	class NetworkReceiver : BroadcastReceiver() {
@@ -67,12 +51,17 @@ class MainActivity : AppCompatActivity() {
 			if (networkInfo?.type == ConnectivityManager.TYPE_WIFI) {
 				refreshBrowser = true
 				Toast.makeText(context, "Wifi is Success", Toast.LENGTH_LONG).show()
+				tvNoInternet.setText("wifi internet")
 			} else if (networkInfo?.type == ConnectivityManager.TYPE_MOBILE) {
 				refreshBrowser = true
 				Toast.makeText(context, "Mobile Internet is Success", Toast.LENGTH_LONG).show()
+				tvNoInternet.setText("mobile internet")
 			} else {
 				refreshBrowser = false
 				Toast.makeText(context, "no connection", Toast.LENGTH_LONG).show()
+				tvNoInternet.setText("no internet")
+
+
 			}
 		}
 
